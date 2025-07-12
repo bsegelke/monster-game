d93f8a4d3a9987e27cb66ae692321c5308d84a89
Brandon Segelke <brandonsegelke@macbook-pro.mynetworksettings.com>
2025-07-12 17:29:58 -0400

    nodemon

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("test")
+const express = require("express");
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log("SERVER IS RUNNING ON PORT 3000")
+})
+
+app.get("/",(req,res)=>{
+    res.send("Server Started")
+} )
